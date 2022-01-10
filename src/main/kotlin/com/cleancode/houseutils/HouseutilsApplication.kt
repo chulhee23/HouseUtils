e45f4a7f2ea0a517cbb282ae87129e709871d007
Chulhee Lee <chulhee23@naver.com>
2022-01-10 16:23:04 +0900

    init

---
+++ src/main/kotlin/com/cleancode/houseutils/HouseutilsApplication.kt
@@ -0,0 +1,11 @@
+package com.cleancode.houseutils
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HouseutilsApplication
+
+fun main(args: Array<String>) {
+	runApplication<HouseutilsApplication>(*args)
+}
